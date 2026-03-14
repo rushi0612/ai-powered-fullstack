40c2f5d58c0a7471414dc1b44a00b26d8d659dcb
Rushikesh <rvp6123@gmail.com>
2026-03-14 23:39:02 +0530

    feat: add blacklist token model with schema for token and timestamp

---
+++ src/models/blacklist.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const blacklistTokenSchema = new mongoose.Schema({
+    token:{
+        type: String,
+        required: [true, 'Token is required to be added in blacklist']
+    }, timestamp: {
+        type: true
+    }
+    
+})
+
+const blacklistTokenModel = mongoose.model('BlacklistTokens', blacklistTokenSchema);
+
+module.exports = blacklistTokenModel;
